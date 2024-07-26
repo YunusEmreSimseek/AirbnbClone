796cd57573267999bfe251efec8eb130b3a55fdd
Yunus Emre Şimşek <yemresimseek@gmail.com>
2024-07-26 13:30:38 +0300

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Emre Simsek on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
